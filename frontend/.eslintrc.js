cc42d13b2e639a385a77358fb0793de9d3abee00
Varma <79473081+jagguvarma15@users.noreply.github.com>
2025-05-28 19:26:45 -0400

    Update

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,21 +1,24 @@
+// frontend/.eslintrc.js
 module.exports = {
   root: true,
-
-  // 1) Files & folders to skip
   ignorePatterns: ["node_modules/", ".next/", "public/"],
 
-  env: { browser: true, es2021: true },
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
     ecmaFeatures: { jsx: true },
-    jsxRuntime: "automatic", // <-- auto import JSX runtime
+    jsxRuntime: "automatic", // ← Tell ESLint you’re on React 17+ auto runtime
   },
-  settings: {
-    react: { version: "detect" }, // <-- auto-detect React version
-  },
+
   plugins: ["react", "@typescript-eslint"],
+
+  settings: {
+    react: {
+      version: "detect", // ← auto-detect React version
+    },
+  },
+
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
@@ -24,19 +27,26 @@ module.exports = {
   ],
 
   rules: {
-    // 2) Allow unused vars if they start with _
+    // 1) No need to import React() in every .tsx
+    "react/react-in-jsx-scope": "off",
+
+    // 2) Unescaped entities are fine in JSX if you prefer
+    "react/no-unescaped-entities": "off",
+
+    // 3) We’re using TS; skip prop-types checks
+    "react/prop-types": "off",
+
+    // 4) Allow Tailwind-style unknown props (like cmdk-input-wrapper)
+    "react/no-unknown-property": "off",
+
+    // 5) Unused vars are OK if prefixed with _
     "@typescript-eslint/no-unused-vars": [
       "error",
       {
         varsIgnorePattern: "^_",
         argsIgnorePattern: "^_",
+        ignoreRestSiblings: true,
       },
     ],
-
-    // 3) You’re on React 17+ with automatic JSX; disable the old rule
-    "react/react-in-jsx-scope": "off",
-
-    // 4) If you prefer not to escape apostrophes in JSX
-    "react/no-unescaped-entities": "off",
   },
 };
